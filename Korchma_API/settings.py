947081347db865216a23c4a2e73421254d351674
vasyl <turokvasil0@gmail.com>
2024-04-27 14:21:04 +0200

    add static

--- Korchma_API/settings.py
+++ Korchma_API/settings.py
@@ -122,11 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "/vol/web/media"
 
+STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
